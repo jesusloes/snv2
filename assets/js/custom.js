9eb72ff7aa63f6c65d05d4a55756d0f18d9ae470
jesus_lopez <jesus.lopez@sonatas.com.mx>
2015-02-20 16:23:53 -0800

    a new hope

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+ $(document).ready(function(){
+ 	$('.parallax').parallax();
+ 	$( '#cbp-qtrotator' ).cbpQTRotator();
+ 	 	$( '#cbp-qtrotator2' ).cbpQTRotator();
+ 	$('.button-collapse').sideNav({menuWidth: 240, activationWidth: 70});
+	$('#jsCarousel').jsCarousel({ autoscroll: true });
+
+ });
